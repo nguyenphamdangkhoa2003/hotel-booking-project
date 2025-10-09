b9ec8ad616de1634ffb7e7adb92b4cc011982cd7
Duong Nhat Thien <dangkhoa2k30@gmail.com>
2025-10-09 22:04:26 +0700

    feat(api): setting up the Swagger Document

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Booking Hotel API')
+    .setDescription('Documenation for Booking Hotel Platform')
+    .setVersion('1.0')
+    .addTag('booking')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 4000);
+
+  Logger.log(`Server run on port: ${process.env.PORT}`);
 }
 bootstrap();
